16c364aa11653f53dd719f6e7650fbd6bba5e745
bouras <marzoukala2018@gmail.com>
2021-09-06 19:07:33 +0100

    minor fixes after removing languages folder

--- admin/includes/templates/sidebar.php
+++ admin/includes/templates/sidebar.php
@@ -3,16 +3,16 @@
   <div class="position-sticky">
     <div class="list-group list-group-flush mx-3 mt-4">
       <a href="dashboard.php" class="list-group-item list-group-item-action py-2 ripple <?php active('dashboard.php') ?>" aria-current="true">
-        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span><?= lang('dashboard'); ?></span>
+        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>dashboard</span>
       </a>
-      <a href="categories.php" class="list-group-item list-group-item-action py-2 ripple <?php active('categories.php'); ?> "><i class="fas fa-list-alt fa-fw me-3"></i><span><?= lang('categories') ?></span></a>
-      <a href="items.php" class="list-group-item list-group-item-action py-2 ripple <?php active('items.php'); ?> "><i class="fas fa-shopping-cart fa-fw me-3"></i><span><?= lang('items') ?></span></a>
-      <a href="users.php" class="list-group-item list-group-item-action py-2 ripple <?php active('users.php') ?>"><i class="fas fa-users fa-fw me-3"></i><span><?= lang(('users')) ?></span></a>
-      <a href="reviews.php" class="list-group-item list-group-item-action py-2 ripple <?php active('reviews.php') ?>"><i class="fas fa-comments fa-fw me-3"></i><span><?= lang(('reviews')) ?></span></a>
+      <a href="categories.php" class="list-group-item list-group-item-action py-2 ripple <?php active('categories.php'); ?> "><i class="fas fa-list-alt fa-fw me-3"></i><span>categories</span></a>
+      <a href="items.php" class="list-group-item list-group-item-action py-2 ripple <?php active('items.php'); ?> "><i class="fas fa-shopping-cart fa-fw me-3"></i><span>items</span></a>
+      <a href="users.php" class="list-group-item list-group-item-action py-2 ripple <?php active('users.php') ?>"><i class="fas fa-users fa-fw me-3"></i><span>users</span></a>
+      <a href="reviews.php" class="list-group-item list-group-item-action py-2 ripple <?php active('reviews.php') ?>"><i class="fas fa-comments fa-fw me-3"></i><span>reviews</span></a>
       <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-        <i class="fas fa-chart-pie fa-fw me-3"></i><span><?= lang('statistics') ?></span>
+        <i class="fas fa-chart-pie fa-fw me-3"></i><span>statistics</span>
       </a>
-      <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-history fa-fw me-3"></i><span><?= lang('logs') ?></span></a>
+      <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-history fa-fw me-3"></i><span>logs</span></a>
       <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-chart-bar fa-fw me-3"></i><span>Orders</span></a>
     </div>
   </div>
